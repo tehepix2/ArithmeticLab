b54bb5ff3ac82b0716600c036e03bbc06bcb84cd
tehepix2 <daniel12landa@gmail.com>
2024-03-26 20:51:05 -0400

    Finished lab

---
+++ Main.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        lib.clearConsole();
+        Stack<String> stack = new Stack<String>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a postfix expression: ");
+        String exp = input.nextLine();
+        Scanner scan = new Scanner(exp);
+        scan.useDelimiter(" ");
+        lib.clearConsole();
+        while (scan.hasNext()) {
+            stack.push(scan.next());
+            switch (stack.peek()) {
+                case "+":
+                    stack.pop();
+                    Integer num2 = Integer.valueOf(stack.pop());
+                    Integer num1 = Integer.valueOf(stack.pop());
+                    stack.push(((Integer)(num1 + num2)).toString());
+                    break;
+
+                case "-":
+                stack.pop();
+                    Integer num4 = Integer.valueOf(stack.pop());
+                    Integer num3 = Integer.valueOf(stack.pop());
+                    stack.push(((Integer)(num3 - num4)).toString());
+                    break;
+
+                case "*":
+                    stack.pop();
+                    Integer num6 = Integer.valueOf(stack.pop());
+                    Integer num5 = Integer.valueOf(stack.pop());
+                    stack.push(((Integer)(num5 * num6)).toString());
+                    break;
+
+                case "/":
+                stack.pop();
+                    Integer num8 = Integer.valueOf(stack.pop());
+                    Integer num7 = Integer.valueOf(stack.pop());
+                    stack.push(((Integer)(num7 / num8)).toString());
+                    break;
+
+                case "%":
+                    stack.pop();
+                    Integer num10 = Integer.valueOf(stack.pop());
+                    Integer num9 = Integer.valueOf(stack.pop());
+                    stack.push(((Integer)(num9 % num10)).toString());
+                    break;
+
+                case "^":
+                    stack.pop();
+                    Integer num12 = Integer.valueOf(stack.pop());
+                    Integer num11 = Integer.valueOf(stack.pop());
+                    stack.push(((Math.pow(num11, num12) + "")));
+                    break;    
+            }
+        }
+       System.out.println("Expression entered: " + exp);
+       System.out.println("Solution: " + stack.peek());
+    }
+}
